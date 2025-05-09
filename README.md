be71fabe937afa6809d8e968e17f64d60d256168
mochou-p <66534926+mochou-p@users.noreply.github.com>
2025-05-09 08:38:38 +0200

    rewrite: crossterm -> libc

--- README.md
+++ README.md
@@ -3,62 +3,6 @@
 # text editor
 simple text-based text editor
 
-# usage examples
-1. open an empty temporary buffer
-```bash
-cargo run
-```
-2. open a file (`Ctrl+S` to save after editing)
-```bash
-cargo run <FILE_PATH>
-```
-3. read from a pipe
-```bash
-# pipe a file
-cat <FILE_PATH> | cargo run
-cargo run < "<FILE_PATH>"
-# or the output of some other command
-echo -e "hello\nworld!!!" | cargo run
-```
-
-# configuration
-file location is in the root of this project `./config.conf`  
-you can ommit settings, and default values will be used  
-when no file is present, a default one like this will be generated:
-```py
-alignment-horizontal = left  # left/center-left/center/center-right/right
-alignment-vertical   = top   # top/center/bottom
-```
-
-# configuration properties
-### alignment-horizontal
-```
-left         center       right
-┌─────────┐  ┌─────────┐  ┌─────────┐
-│#####    │  │  #####  │  │    #####│
-│###      │  │   ###   │  │      ###│
-│#        │  │    #    │  │        #│
-│###      │  │   ###   │  │      ###│
-└─────────┘  └─────────┘  └─────────┘
-
-center-left  center-right
-┌─────────┐  ┌─────────┐
-│  #####  │  │  #####  │
-│  ###    │  │    ###  │
-│  #      │  │      #  │
-│  ###    │  │    ###  │
-└─────────┘  └─────────┘
-```
-### alignment-vertical
-```
-top          center       bottom
-┌─────────┐  ┌─────────┐  ┌─────────┐
-│#########│  │         │  │         │
-│         │  │#########│  │         │
-│         │  │         │  │#########│
-└─────────┘  └─────────┘  └─────────┘
-```
-
 ## License
 Licensed under either of
  * Apache License, Version 2.0  
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,134 @@
 // text-editor/src/main.rs
 
-#![expect(clippy::multiple_crate_versions, reason = "inside crossterm")]
+use libc::{
+    ioctl, iscntrl, tcgetattr, tcsetattr, winsize,
+    BRKINT, CS8, ECHO, ICANON, ICRNL, IEXTEN, INPCK, ISIG, ISTRIP, IXON, OPOST, STDIN_FILENO, STDOUT_FILENO, TCSAFLUSH, TIOCGWINSZ
+};
 
-mod config;
-mod editor;
-mod utils;
+use std::{
+    io::{stdin, stdout, Error, Read as _, Write as _},
+    mem::zeroed
+};
 
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    editor::TextEditor::new()?
-        .run()
+static CSI: &str = "\x1b[";
+
+static FG: &str = "38;2;";
+static BG: &str = "48;2;";
+
+// Catppuccin Mocha
+static BASE:      &str = "30;30;46m";
+static SURFACE_0: &str = "49;50;68m";
+static SUBTEXT_0: &str = "166;173;200m";
+static TEXT:      &str = "205;214;244m";
+
+fn main() {
+    let mut size: winsize = unsafe { zeroed() };
+
+    if unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ, &mut size) } != 0 {
+        eprintln!(
+            "{CSI}31merror{CSI}0m: `libc::ioctl` returned \"{}\"",
+            Error::last_os_error()
+        );
+        return;
+    }
+
+    let mut original_termios = unsafe { zeroed() };
+
+    if unsafe { tcgetattr(STDIN_FILENO, &mut original_termios) } != 0 {
+        eprintln!(
+            "{CSI}31merror{CSI}0m: `libc::tcgetattr` returned \"{}\"",
+            Error::last_os_error()
+        );
+        return;
+    }
+
+    let mut new_termios  = original_termios;
+    new_termios.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    new_termios.c_oflag &= !(OPOST);
+    new_termios.c_cflag |=   CS8;
+    new_termios.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
+
+    let mut stdin  = stdin();
+    let mut stdout = stdout();
+    let mut buffer = [0u8; 1];
+
+    if unsafe { tcsetattr(STDIN_FILENO, TCSAFLUSH, &new_termios) } != 0 {
+        eprintln!(
+            "{CSI}31merror{CSI}0m: `libc::tcsetattr` returned \"{}\"",
+            Error::last_os_error()
+        );
+        return;
+    }
+
+    hide();
+    alternative_screen();
+    clear();
+    move_to(1, 1);
+
+    let filename = "<unnamed file>";
+    let stamp    = format!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    print!(
+        "{CSI}{BG}{SURFACE_0}{CSI}{FG}{SUBTEXT_0}{filename}{}{stamp}{CSI}{BG}{BASE}{CSI}{FG}{TEXT}{}",
+        " ".repeat(size.ws_col as usize - (filename.len() + stamp.len())),
+        " ".repeat((size.ws_col * (size.ws_row - 1)) as usize)
+    );
+
+    move_to(1, 2);
+    show();
+
+    loop {
+        let _ = stdout.flush();
+
+        match stdin.read_exact(&mut buffer) {
+            Ok(()) => {
+                if unsafe { iscntrl(i32::from(buffer[0])) } == 0 {
+                    print!("{}", char::from(buffer[0]));
+                } else {
+                    match buffer[0] {
+                        13 => {  // CR
+                            move_down(1);
+                            let _ = stdout.flush();
+                        },
+                        27 => {  // Escape
+                            // TODO: also other stuff report 27,
+                            //       like the start of sequences
+                            //       for PageUp/Delete/...
+                            break;
+                        },
+                        127 => {  // Backspace
+                            // TODO: check if there even is something to erase
+                            move_left(1);
+                            clear_to_end();
+                            let _ = stdout.flush();
+                        },
+                        _ => {
+                            print!("{CSI}30m[{}]{CSI}0m", buffer[0]);
+                        }
+                    }
+                }
+            },
+            Err(err) => {
+                normal_screen();
+                unsafe { tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) };
+                eprintln!("{CSI}31merror{CSI}0m: `Stdin::read_exact returned \"{err}\"");
+                return;
+            }
+        }
+    }
+
+    print!("{CSI}0m");
+    normal_screen();
+    unsafe { tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) };
 }
+
+fn show()                  { print!("{CSI}?25h");     }
+fn hide()                  { print!("{CSI}?25l");     }
+fn alternative_screen()    { print!("{CSI}?1049h");   }
+fn normal_screen()         { print!("{CSI}?1049l");   }
+fn clear_to_end()          { print!("{CSI}0J");       }
+fn clear()                 { print!("{CSI}2J");       }
+fn move_to(x: u16, y: u16) { print!("{CSI}{y};{x}H"); }
+fn move_left(d: u16)       { print!("{CSI}{d}D");     }
+fn move_down(d: u16)       { print!("{CSI}{d}E");     }
 
