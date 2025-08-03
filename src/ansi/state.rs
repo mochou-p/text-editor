64d286454f8fa7002948a14dcced0ba9811ad83c
mochou-p <66534926+mochou-p@users.noreply.github.com>
2025-08-03 15:05:13 +0200

    parse incoming bytes into Commands

--- src/ansi/state.rs
+++ src/ansi/state.rs
@@ -6,3 +6,6 @@ use super::CSI;
 pub fn alternative_screen() { print!("{CSI}?1049h"); }
 pub fn      normal_screen() { print!("{CSI}?1049l"); }
 
+pub fn  enable_mouse() { print!("{CSI}?1006h{CSI}?1003h"); }
+pub fn disable_mouse() { print!("{CSI}?1003l{CSI}?1006l"); }
+
--- src/editor.rs
+++ src/editor.rs
@@ -3,11 +3,11 @@
 use libc::{
     self,
     fcntl, iscntrl, tcgetattr, tcsetattr,
-    BRKINT, CS8, ECHO, F_GETFL, F_SETFL, ICANON, ICRNL, IEXTEN, INPCK, ISIG, ISTRIP, IXON, O_NONBLOCK, OPOST, STDIN_FILENO, STDOUT_FILENO, TCSAFLUSH
+    CREAD, CS8, F_GETFL, F_SETFL, O_NONBLOCK, STDIN_FILENO, STDOUT_FILENO, TCSAFLUSH
 };
 
 use std::{
-    io::{self, stdin, stdout, Read as _, Write as _},
+    io::{self, stdin, Stdin, stdout, Stdout, Read as _, Write as _},
     mem::zeroed
 };
 
@@ -17,45 +17,100 @@ use crate::ansi::{cursor, state, clear};
 pub struct Editor {
 }
 
+#[expect(dead_code, reason = "temporarily only printing")]
+#[derive(Debug)]
+enum Command {
+    Print(u8),
+    Escape,
+    Tab,
+    Backspace,
+    Delete,
+    Enter,
+    ArrowUp,
+    ArrowDown,
+    ArrowRight,
+    ArrowLeft,
+    ShiftTab,
+    ShiftEnter,
+    ShiftArrowUp,
+    ShiftArrowDown,
+    ShiftArrowRight,
+    ShiftArrowLeft,
+    CtrlA,
+    CtrlC,
+    CtrlQ,
+    CtrlR,
+    CtrlS,
+    CtrlV,
+    CtrlX,
+    CtrlY,
+    CtrlZ,
+    CtrlBackspace,
+    CtrlDelete,
+    CtrlArrowUp,
+    CtrlArrowDown,
+    CtrlArrowRight,
+    CtrlArrowLeft,
+    MouseLeftPress      (u16, u16),
+    MouseLeftDrag       (u16, u16),
+    MouseLeftRelease    (u16, u16),
+    MouseRightPress     (u16, u16),
+    MouseRightDrag      (u16, u16),
+    MouseRightRelease   (u16, u16),
+    MouseMiddlePress    (u16, u16),
+    MouseMiddleDrag     (u16, u16),
+    MouseMiddleRelease  (u16, u16),
+    MouseForwardPress   (u16, u16),
+    MouseForwardDrag    (u16, u16),
+    MouseForwardRelease (u16, u16),
+    MouseBackPress      (u16, u16),
+    MouseBackDrag       (u16, u16),
+    MouseBackRelease    (u16, u16),
+    MouseHover          (u16, u16),
+    ScrollUp            (u16, u16),
+    ScrollDown          (u16, u16),
+    Exit,
+    Nop,
+    Error(io::Error)
+}
+
 impl Editor {
     pub fn run() -> io::Result<()> {
-        let original_termios = get_termios()?;
-        let      new_termios = prepare_termios(original_termios);
-
-        let mut stdin  = stdin();
+        let mut error  = None;
+        let mut buffer = [0u8;  1];
+        let mut trail  = [0u8; 32];
+        let     stdin  = stdin();
         let mut stdout = stdout();
 
-        set_termios(new_termios)?;
-        state::alternative_screen();
-        clear::whole_screen();
-        cursor::move_to(1, 1);
-        let _ = stdout.flush();
-
-        let mut buffer = [0u8; 1];
-        let mut error  = None;
+        let original_termios = prepare_terminal(&mut stdout)?;
 
         loop {
-            match stdin.read_exact(&mut buffer) {
-                Ok(()) => {
-                    if buffer[0] == 3 {
-                        break;
-                    }
-
-                    print!("{buffer:?} ; {}", unsafe { iscntrl(i32::from(buffer[0])) });
-                    cursor::move_to_next_line(1);
-                    let _ = stdout.flush();
+            match blocking_read_to_command(&stdin, &mut buffer, &mut trail) {
+                Command::Escape => {
+                    break;
                 },
-                Err(err) => {
+                Command::Error(err) => {
                     error = Some(err);
+                    break;
+                },
+                #[cfg(debug_assertions)]
+                other => {
+                    print!("{other:?} -> {{{buffer:?}, {trail:?}}}                         ");
+                    cursor::move_to_next_line(1);
+                    stdout.flush()?;
                 }
+                #[cfg(not(debug_assertions))]
+                _ => ()
             }
+
+            buffer = [0u8;  1];
+            trail  = [0u8; 32];
         }
 
-        state::normal_screen();
-        set_termios(original_termios)?;
+        restore_terminal(original_termios, &mut stdout)?;
 
         if let Some(err) = error {
-            eprintln!("\x1b[31merror:\x1b[0m {err}");
+            eprintln!("{err}");
         }
 
         Ok(())
@@ -72,11 +127,11 @@ fn get_termios() -> io::Result<libc::termios> {
     Ok(termios)
 }
 
-const fn prepare_termios(mut termios: libc::termios) -> libc::termios {
-    termios.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    termios.c_oflag &= !(OPOST);
-    termios.c_cflag |=   CS8;
-    termios.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
+const fn raw_termios(mut termios: libc::termios) -> libc::termios {
+    termios.c_iflag = 0; // &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON | INLCR | IGNCR);
+    termios.c_oflag = 0; // &= !(OPOST);
+    termios.c_cflag = CREAD | CS8;
+    termios.c_lflag = 0; // &= !(ECHO | ICANON | IEXTEN | ISIG);
 
     termios
 }
@@ -89,23 +144,289 @@ fn set_termios(termios: libc::termios) -> io::Result<()> {
     Ok(())
 }
 
-fn _non_blocking_read<const N: usize>(buf: &mut [u8; N]) -> io::Result<bool> {
-    let flags = unsafe { fcntl(STDIN_FILENO, F_GETFL) };
-    if flags == -1 {
+fn prepare_terminal(stdout: &mut Stdout) -> io::Result<libc::termios> {
+    let original_termios = get_termios()?;
+    let      new_termios = raw_termios(original_termios);
+
+    set_termios(new_termios)?;
+    state::alternative_screen();
+    state::enable_mouse();
+    clear::whole_screen();
+    cursor::move_to(1, 1);
+
+    stdout.flush()?;
+
+    Ok(original_termios)
+}
+
+fn restore_terminal(termios: libc::termios, stdout: &mut Stdout) -> io::Result<()> {
+    state::normal_screen();
+    state::disable_mouse();
+    set_termios(termios)?;
+
+    stdout.flush()?;
+
+    Ok(())
+}
+
+enum MouseEventParsingStage {
+    Action,
+    X,
+    Y,
+    Finished(u8)
+}
+
+#[expect(clippy::too_many_lines)]
+fn parse_mouse_event(bytes: &[u8]) -> Option<Command> {
+    let mut action = 0;
+    let mut x      = 0;
+    let mut y      = 0;
+    let mut stage  = MouseEventParsingStage::Action;
+
+    for byte in bytes {
+        match stage {
+            MouseEventParsingStage::Action => {
+                if *byte == 59 {
+                    stage = MouseEventParsingStage::X;
+                } else {
+                    action = action * 10 + u16::from(byte - 48);
+                }
+            },
+            MouseEventParsingStage::X => {
+                if *byte == 59 {
+                    stage = MouseEventParsingStage::Y;
+                } else {
+                    x = x * 10 + u16::from(byte - 48);
+                }
+            },
+            MouseEventParsingStage::Y => {
+                if *byte == 77 || *byte == 109 {
+                    stage = MouseEventParsingStage::Finished(*byte);
+                    break;
+                }
+
+                y = y * 10 + u16::from(byte - 48);
+            },
+            MouseEventParsingStage::Finished(_) => ()
+        }
+    }
+
+    if let MouseEventParsingStage::Finished(last_byte) = stage {
+        match action {
+            0 => {
+                match last_byte {
+                    77  => { return Some(Command::MouseLeftPress   (x, y)); },
+                    109 => { return Some(Command::MouseLeftRelease (x, y)); },
+                    _   => ()
+                }
+            },
+            1 => {
+                match last_byte {
+                    77  => { return Some(Command::MouseMiddlePress   (x, y)); },
+                    109 => { return Some(Command::MouseMiddleRelease (x, y)); },
+                    _   => ()
+                }
+            },
+            2 => {
+                match last_byte {
+                    77  => { return Some(Command::MouseRightPress   (x, y)); },
+                    109 => { return Some(Command::MouseRightRelease (x, y)); },
+                    _   => ()
+                }
+            },
+            32 => {
+                if last_byte == 77 {
+                    return Some(Command::MouseLeftDrag(x, y));
+                }
+            },
+            33 => {
+                if last_byte == 77 {
+                    return Some(Command::MouseMiddleDrag(x, y));
+                }
+            },
+            34 => {
+                if last_byte == 77 {
+                    return Some(Command::MouseRightDrag(x, y));
+                }
+            },
+            35 => {
+                if last_byte == 77 {
+                    return Some(Command::MouseHover(x, y));
+                }
+            },
+            64 => {
+                if last_byte == 77 {
+                    return Some(Command::ScrollUp(x, y));
+                }
+            },
+            65 => {
+                if last_byte == 77 {
+                    return Some(Command::ScrollDown(x, y));
+                }
+            },
+            128 => {
+                match last_byte {
+                    77  => { return Some(Command::MouseBackPress   (x, y)); },
+                    109 => { return Some(Command::MouseBackRelease (x, y)); },
+                    _   => ()
+                }
+            },
+            129 => {
+                match last_byte {
+                    77  => { return Some(Command::MouseForwardPress   (x, y)); },
+                    109 => { return Some(Command::MouseForwardRelease (x, y)); },
+                    _   => ()
+                }
+            },
+            160 => {
+                if last_byte == 77 {
+                    return Some(Command::MouseBackDrag(x, y));
+                }
+            },
+            161 => {
+                if last_byte == 77 {
+                    return Some(Command::MouseForwardDrag(x, y));
+                }
+            },
+            _ => ()
+        }
+    }
+
+    None
+}
+
+// TODO: bytes of more than just one event could be read at one time, this is currently not handled
+//       i could just read more and more conditionally based on previous bytes, instead of always 32
+#[expect(clippy::too_many_lines)]
+fn blocking_read_to_command<const N: usize>(mut stdin: &Stdin, buffer: &mut [u8; 1], trail: &mut [u8; N]) -> Command {
+    // TODO: could this be shortened by checking bit flags and indexing into a multidimensional array? /shrug
+    match stdin.read_exact(buffer) {
+        Ok(()) => {
+            let cntrl = unsafe { iscntrl(i32::from(buffer[0])) };
+            match cntrl {
+                0 => { return Command::Print(buffer[0]); },
+                2 => {
+                    match buffer[0] {
+                        1  => { return Command::CtrlA;         },
+                        3  => { return Command::CtrlC;         },
+                        8  => { return Command::CtrlBackspace; },
+                        9  => { return Command::Tab;           },
+                        13 => { return Command::Enter;         },
+                        17 => { return Command::CtrlQ;         },
+                        18 => { return Command::CtrlR;         },
+                        19 => { return Command::CtrlS;         },
+                        22 => { return Command::CtrlV;         },
+                        24 => { return Command::CtrlX;         },
+                        25 => { return Command::CtrlY;         },
+                        26 => { return Command::CtrlZ;         },
+                        27 => {
+                            match non_blocking_read(trail) {
+                                Ok(result) => {
+                                    match result {
+                                        Ok(read_count) => {
+                                            if trail[0..2] == [91, 60] {
+                                                if let Some(command) = parse_mouse_event(&trail[2..read_count]) {
+                                                    return command;
+                                                }
+                                            }
+
+                                            match read_count {
+                                                2 => {
+                                                    if trail[0] == 91 {
+                                                        match trail[1] {
+                                                            65 => { return Command::ArrowUp;    },
+                                                            66 => { return Command::ArrowDown;  },
+                                                            67 => { return Command::ArrowRight; },
+                                                            68 => { return Command::ArrowLeft;  },
+                                                            90 => { return Command::ShiftTab;   },
+                                                            _  => ()
+                                                        }
+                                                    }
+                                                },
+                                                3 => {
+                                                    if trail[0..3] == [91, 51, 126] {
+                                                        return Command::Delete;
+                                                    }
+                                                },
+                                                5 => {
+                                                    if trail[0..5] == [91, 51, 59, 53, 126] {
+                                                        return Command::CtrlDelete;
+                                                    }
+
+                                                    if trail[0..3] == [91, 49, 59] {
+                                                        match trail[3] {
+                                                            50 => {
+                                                                match trail[4] {
+                                                                    65 => { return Command::ShiftArrowUp;    },
+                                                                    66 => { return Command::ShiftArrowDown;  },
+                                                                    67 => { return Command::ShiftArrowRight; },
+                                                                    68 => { return Command::ShiftArrowLeft;  },
+                                                                    _  => ()
+                                                                }
+                                                            },
+                                                            53 => {
+                                                                match trail[4] {
+                                                                    65 => { return Command::CtrlArrowUp;    },
+                                                                    66 => { return Command::CtrlArrowDown;  },
+                                                                    67 => { return Command::CtrlArrowRight; },
+                                                                    68 => { return Command::CtrlArrowLeft;  },
+                                                                    _  => ()
+                                                                }
+                                                            },
+                                                            _ => ()
+                                                        }
+                                                    }
+                                                },
+                                                9 => {
+                                                    if trail[0..9] == [91, 50, 55, 59, 50, 59, 49, 51, 126] {
+                                                        return Command::ShiftEnter;
+                                                    }
+                                                },
+                                                _ => ()
+                                            }
+                                        },
+                                        Err(_) => {
+                                            return Command::Escape;
+                                        }
+                                    }
+                                },
+                                Err(err) => {
+                                    return Command::Error(err);
+                                }
+                            }
+                        },
+                        127 => { return Command::Backspace; },
+                        _   => ()
+                    }
+                },
+                _ => ()
+            }
+
+            Command::Nop
+        },
+        Err(err) => {
+            Command::Error(err)
+        }
+    }
+}
+
+fn non_blocking_read<const N: usize>(buffer: &mut [u8; N]) -> io::Result<io::Result<usize>> {
+    let original_flags = unsafe { fcntl(STDIN_FILENO, F_GETFL) };
+    if original_flags == -1 {
         return Err(io::Error::last_os_error());
     }
 
-    let new_flags = flags | O_NONBLOCK;
+    let new_flags = original_flags | O_NONBLOCK;
     if unsafe { fcntl(STDOUT_FILENO, F_SETFL, new_flags) } == -1 {
         return Err(io::Error::last_os_error());
     }
 
-    let ret = stdin().read(buf).is_ok();
+    let result = stdin().read(buffer);
 
-    if unsafe { fcntl(STDOUT_FILENO, F_SETFL, flags) } == -1 {
+    if unsafe { fcntl(STDOUT_FILENO, F_SETFL, original_flags) } == -1 {
         return Err(io::Error::last_os_error());
     }
 
-    Ok(ret)
+    Ok(result)
 }
 
