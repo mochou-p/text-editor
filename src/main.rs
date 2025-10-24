10dc039d6a2bb40de2e9198c43f751bb91c3f068
mochou-p <66534926+mochou-p@users.noreply.github.com>
2025-10-24 15:40:06 +0200

    ah poop, here we go again

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 // text-editor/src/main.rs
 
-#[cfg(not(target_os = "linux"))]
-compile_error!("this project currently only supports Linux");
-
-mod ansi;
-mod editor;
-
-
-fn main() -> std::io::Result<()> {
-    editor::Editor::new().run()
+fn main() {
+    println!(":D");
 }
 
